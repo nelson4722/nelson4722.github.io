09d8af4be10645d66f701565155a868b7fe74130
Nelson Alegria <nelson.alegria@femsasalud.com>
2025-09-02 09:59:58 -0400

    agregar cambios

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Servir archivos estáticos
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "", "index.html"));
 });
 
 app.listen(PORT, () => {
